2ebf192b74f19238dcf1d83a73730251e1ce5c28
buser23 <bf.42porto@gmail.com>
2023-04-13 17:59:11 +0100

    Commit

--- libft.h
+++ libft.h
@@ -6,18 +6,18 @@
 /*   By: bsalgado <bsalgado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 15:39:58 by bsalgado          #+#    #+#             */
-/*   Updated: 2023/04/13 15:40:02 by bsalgado         ###   ########.fr       */
+/*   Updated: 2023/04/13 17:56:51 by bsalgado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
 
-int ft_isalpha(int c);
+int	ft_isalpha(int c);
 
 #endif
--- main.c
+++ main.c
@@ -6,15 +6,17 @@
 /*   By: bsalgado <bsalgado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 15:39:49 by bsalgado          #+#    #+#             */
-/*   Updated: 2023/04/13 16:25:08 by bsalgado         ###   ########.fr       */
+/*   Updated: 2023/04/13 17:53:41 by bsalgado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int main(void)
+int	main(void)
 {
-	char name  = 'b';
+	char	name;
+
+	name = 'b';
 	printf("\n\t>>> USING isalpha() <<<\n\n");
 	printf("Sending \'%c\'%d\n", name, ft_isalpha(name));
 }
